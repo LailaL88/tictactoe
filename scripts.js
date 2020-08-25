bb793cb0219cb6ad10561ab2faf895e135e8d58b
LailaL88 <58104146+LailaL88@users.noreply.github.com>
2020-08-25 21:27:28 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function onPageLoaded() {
     const swithTurn = document.getElementById("gameOverText");
     const restartButton = document.querySelector(".button-container button");
     const gameField = document.getElementById("gameField");
+    const gameBody = document.querySelector("body");
     const cell1 = document.getElementById("cell1");
     const cell2 = document.getElementById("cell2");
     const cell3 = document.getElementById("cell3");
@@ -27,6 +28,7 @@ function onPageLoaded() {
             if (cell.innerText == "X" || cell.innerText == "O") {
                 return;
             }
+            cell.style.backgroundColor = "lightgreen";
             if (getTurn()) {
                 cell.innerText = "X";
                 swithTurn.innerText = "It's O turn";
@@ -38,31 +40,24 @@ function onPageLoaded() {
                 cellArr.push("O");
             }
 
-            getWinnerX(cell1, cell2, cell3);
-            getWinnerX(cell4, cell5, cell6);
-            getWinnerX(cell7, cell8, cell9);
-            getWinnerX(cell1, cell4, cell7);
-            getWinnerX(cell2, cell5, cell8);
-            getWinnerX(cell3, cell6, cell9);
-            getWinnerX(cell1, cell5, cell9);
-            getWinnerX(cell3, cell5, cell7);
-
-            getWinnerO(cell1, cell2, cell3);
-            getWinnerO(cell4, cell5, cell6);
-            getWinnerO(cell7, cell8, cell9);
-            getWinnerO(cell1, cell4, cell7);
-            getWinnerO(cell2, cell5, cell8);
-            getWinnerO(cell3, cell6, cell9);
-            getWinnerO(cell1, cell5, cell9);
-            getWinnerO(cell3, cell5, cell7);
+            getWinner(cell1, cell2, cell3);
+            getWinner(cell4, cell5, cell6);
+            getWinner(cell7, cell8, cell9);
+            getWinner(cell1, cell4, cell7);
+            getWinner(cell2, cell5, cell8);
+            getWinner(cell3, cell6, cell9);
+            getWinner(cell1, cell5, cell9);
+            getWinner(cell3, cell5, cell7);
 
             if (gameOver) {
                 gameField.addEventListener("click", stopClick, true);
                 gameOverText.innerText = "Game Over!";
+                gameBody.style.backgroundColor = "indianred";
             } else if (cellArr.length > 8) {
                 gameField.addEventListener("click", stopClick, true);
                 gameOverText.innerText = "Game Over!";
                 winner.innerText = "There is no winner!";
+                gameBody.style.backgroundColor = "indianred";
             }
         })
     })
@@ -74,8 +69,10 @@ function onPageLoaded() {
         gameOver = false;
         swithTurn.innerText = "It's X turn";
         winner.innerText = "";
+        gameBody.style.backgroundColor = "white";
         cells.forEach((cell) => {
             cell.innerText = "";
+            cell.style.backgroundColor = "azure";
         })
     }
 
@@ -91,19 +88,18 @@ function onPageLoaded() {
         }
     }
 
-    function getWinnerX(cell1, cell2, cell3) {
+    function getWinner(cell1, cell2, cell3) {
         if (cell1.innerText == "X" && cell2.innerText == "X" && cell3.innerText == "X") {
             winner.innerText = " X wins!";
             gameOver = true;
         }
-    }
-
-    function getWinnerO(cell1, cell2, cell3) {
         if (cell1.innerText == "O" && cell2.innerText == "O" && cell3.innerText == "O") {
             winner.innerText = " O wins!";
             gameOver = true;
         }
     }
+
+
 }
 
 document.addEventListener("DOMContentLoaded", onPageLoaded);
